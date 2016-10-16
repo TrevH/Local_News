795e1a0d186fee732edcfc9a4a1ffd7921c695d6
Shiva Balachandran <shiva.balachandran@uqconnect.edu.au>
2016-10-16 16:10:46 +1000

    TouchTest2
    Our trial code for touch, It didn’t work.

---
+++ touchtest2/Assets/button.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class button : MonoBehaviour {
+
+	public Color defaultColor;
+	public Color selectedColor;
+
+	private Material mat;
+
+	void Start() {
+		mat = GetComponent<Renderer>().material;
+	}
+
+	void onTouchDown() {
+		mat.color = selectedColor;
+	}
+
+	void onTouchUp() {
+		mat.color = defaultColor;
+	}
+
+	void onTouchMove() {
+		mat.color = selectedColor;
+	}
+
+	void onTouchExit() {
+		mat.color = defaultColor;
+	}
+
+}
---
+++ touchtest2/Assets/touchInput.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class touchInput : MonoBehaviour {
+
+	public LayerMask touchInputMask;
+
+	private List<GameObject> touchList = new List<GameObject>();
+	private GameObject[] touchesOld;
+	private RaycastHit hit;
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+#if UNITY_EDITOR
+
+		if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0))
+		{
+			touchesOld = new GameObject[touchList.Count];
+			touchList.CopyTo (touchesOld);
+			touchList.Clear ();
+
+		
+			Ray ray = GetComponent<Camera>().ScreenPointToRay (Input.mousePosition);
+
+			if (Physics.Raycast(ray, out hit, touchInputMask)) {
+				GameObject recipient = hit.transform.gameObject;
+				touchList.Add(recipient);
+
+				if (Input.GetMouseButtonDown(0))
+				{
+					recipient.SendMessage("onTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+				}
+				if (Input.GetMouseButtonUp(0))
+				{
+					recipient.SendMessage("onTouchUp", hit.point, SendMessageOptions.DontRequireReceiver);
+				}
+				if (Input.GetMouseButton(0))
+				{
+					recipient.SendMessage("onTouchMove", hit.point, SendMessageOptions.DontRequireReceiver);
+				}
+			}
+					
+
+			foreach (GameObject g in touchesOld)
+			{
+				if (!touchList.Contains(g))
+				{
+					g.SendMessage("onTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+				}
+			}
+		}
+
+#endif
+		if (Input.touchCount > 0)
+		{
+			touchesOld = new GameObject[touchList.Count];
+			touchList.CopyTo (touchesOld);
+			touchList.Clear ();
+
+			foreach (Touch touch in Input.touches) 
+			{
+				Ray ray = GetComponent<Camera>().ScreenPointToRay (touch.position);
+
+				if (Physics.Raycast(ray, out hit, touchInputMask)) {
+						GameObject recipient = hit.transform.gameObject;
+						touchList.Add(recipient);
+
+						if (touch.phase == TouchPhase.Began)
+						{
+							recipient.SendMessage("onTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+						}
+						if (touch.phase == TouchPhase.Ended)
+						{
+							recipient.SendMessage("onTouchUp", hit.point, SendMessageOptions.DontRequireReceiver);
+						}
+						if (touch.phase == TouchPhase.Moved)
+						{
+							recipient.SendMessage("onTouchMove", hit.point, SendMessageOptions.DontRequireReceiver);
+						}
+						if (touch.phase == TouchPhase.Canceled)
+						{
+							recipient.SendMessage("onTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+						}
+				}
+						
+			}
+			
+					foreach (GameObject g in touchesOld)
+					{
+						if (!touchList.Contains(g))
+						{
+							g.SendMessage("onTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+						}
+					}
+		}
+	}
+}
