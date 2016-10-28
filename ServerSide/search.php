2ecfbf8e31096b0680738d89b2e0bb4a98c350a0
dayrek <wtfnuubs1337@hotmail.com>
2016-10-28 23:42:40 +1000

    Test

--- ServerSide/search.php
+++ ServerSide/search.php
@@ -1,24 +1,30 @@
 <?php
-$servername = "";
-$username = "";
-$password = "";
-$dbname = "";
+require_once 'lib/twitteroauth.php';
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if (mysqli_connect_errno($con))
+$tField1 = $_GET['term'];
+$tField2 = $_GET['geocode'];
+ 
+define('CONSUMER_KEY', 'LQ9n9fhM4TGUWLPyx25x5CmV8');
+define('CONSUMER_SECRET', 'xxMoLbe4j2qP1N5LQU6w6uvUqmwoNjG5tbElCCHAFQXEWcHs8G');
+define('ACCESS_TOKEN', '783502369392762880-SsMXmqgzYha0I4tdNXcKdQzYNN9qYOS');
+define('ACCESS_TOKEN_SECRET', '2a36fDal4rjZ3EMdbXZZ1QSeFuok4XHmi5unIqGbZ4uSp');
+ 
+function search(array $query)
 {
-   echo '{"query_result":"ERROR"}';
+  $toa = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+  return $toa->get('search/tweets', $query);
 }
  
-$result = mysqli_query($conn,"TRUNCATE TABLE site");
+$query = array(
+  "q" => $tField1,
+  "lang" => "en",
+  "geocode" => $tField2,
+  “exclude” => “retweets”
+);
 
-if($result == true) {
-    echo '{"query_result":"SUCCESS"}';
+$results = search($query);
+ 
+foreach ($results->statuses as $result) {
+  echo $result->user->profile_image_url . " :? " . $result->user->screen_name . " :? " . $result->text . " :? ";
 }
-else{
-    echo '{"query_result":"FAILURE"}';
-}
-$conn->close();
-?>
+
